f9b57c3e40f148267409c900c4b4748a0865ede4
Freeman <Freeman.Trader@wsu.edu>
2022-11-30 22:11:41 -0800

    Edited BasePlantClass

--- Pong/BasePlantClass.hpp
+++ Pong/BasePlantClass.hpp
@@ -9,7 +9,9 @@ class BasePlantClass
 private:
 	std::string mName;
 	int mHealth;
-	unsigned int mDamage;
+	bool mPassive;
+	//unsigned int mDamage; //For class that inherits this class
+	//unsigned int mFireRate;
 	float mXPosition;
 	float mYPosition;
 	sf::Texture mPlantTexture;
@@ -17,15 +19,17 @@ private:
 
 public:
 	BasePlantClass();
-	BasePlantClass(std::string const nName, int const nHealth, unsigned int const nDamage,
-		float const nXPosition, float const nYPosition,
-		sf::Texture const nPlantTexture, sf::Sprite const nPlantSprite);
+	BasePlantClass(std::string const nName, int const nHealth, bool const nPassive, 
+		unsigned int const nDamage, unsigned int const nFireRate, float const nXPosition,
+		float const nYPosition, sf::Texture const nPlantTexture, sf::Sprite const nPlantSprite);
 	~BasePlantClass();
 
 	//Setters
 	void setName(std::string const nName);
 	void setHealth(int const nHealth);
+	void setPassive(bool const nPassive);
 	void setDamage(unsigned int const nDamage);
+	void setFireRate(unsigned int const nFireRate);
 	void setXPosition(float const nXPosition);
 	void setYPosition(float const nYPosition);
 	void setPlantTexture(sf::Texture const nPlantTexture);
@@ -34,13 +38,18 @@ public:
 	//Getters
 	std::string getName(void) const;
 	int getHealth(void) const;
+	bool getPassive(void) const;
 	unsigned int getDamage(void) const;
+	unsigned int getFireRate(void) const;
 	float getXPosition(void) const;
 	float getYPosition(void) const;
 	sf::Texture getPlantTexture(void) const;
 	sf::Sprite getPlantSprite(void) const;
 
-
+	//Functions
+	//virtual bool checkFire(void) = 0; Might be in certain class
+	//virtual void firePod(void) = 0;
+	virtual void checkAlive(void) = 0;
 
 };
 
