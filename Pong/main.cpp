d8ca3afacc5d6c438b0704311d26e28c96cb4912
Alyssa Lammi <alyssa.lammi@wsu.edu>
2022-12-04 13:35:23 -0800

    Half working mouse and clock added

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -1,34 +1,40 @@
 
-#include <SFML/Graphics.hpp>
+// #include <SFML/Graphics.hpp>
 
-#define WINDOWS_WIDTH 500
-#define WINDOWS_HEIGHT 500
+#include "ZVP.h"
 
-#define MOVEMENT_SPEED .2f
+//#define WINDOWS_WIDTH 500
+//#define WINDOWS_HEIGHT 500
+//
+//#define MOVEMENT_SPEED .2f
 
 int main()
 {
-	unsigned int direction = 0;
+	ZVP game;
+
+	game.runGame();
+
+	//unsigned int direction = 0;
 
 	//Window
-	sf::VideoMode defaultVM(WINDOWS_WIDTH, WINDOWS_HEIGHT);
-	sf::RenderWindow Window(defaultVM, "Zombies Vs Plants", sf::Style::Default);
+	//sf::VideoMode defaultVM(WINDOWS_WIDTH, WINDOWS_HEIGHT);
+	//sf::RenderWindow Window(defaultVM, "Zombies Vs Plants", sf::Style::Default);
 
 
 	//Title - Currently needs a font .ttf file to work with
-	sf::Font defaultFont;
-	if(!defaultFont.loadFromFile("CrackMan.TTF"))
-		{return -1;}
-	sf::Text titleName("Zombies Vs Plants", defaultFont, 25);
-	titleName.setColor(sf::Color::Cyan);
+	//sf::Font defaultFont;
+	//if(!defaultFont.loadFromFile("CrackMan.TTF"))
+	//	{return -1;}
+	//sf::Text titleName("Zombies Vs Plants", defaultFont, 25);
+	//titleName.setColor(sf::Color::Cyan);
 	
 
 	//Pacman Object
-	sf::CircleShape shape;
-	shape.setRadius(50.f);
-	shape.setOutlineThickness(5.f);
-	shape.setFillColor(sf::Color::Yellow);
-	shape.setOutlineColor(sf::Color::Red);
+	//sf::CircleShape shape;
+	//shape.setRadius(50.f);
+	//shape.setOutlineThickness(5.f);
+	//shape.setFillColor(sf::Color::Yellow);
+	//shape.setOutlineColor(sf::Color::Red);
 
 	//For Sprites
 	/*sf::Texture PacManTexture;
@@ -40,53 +46,54 @@ int main()
 	PacManSprite.setTexture(PacManTexture);*/
 
 
-	while (Window.isOpen())
-	{
-		sf::Event event;
-		while (Window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				Window.close();
-		}
+	//while (Window.isOpen())
+	//{
+	//	sf::Event event;
+	//	while (Window.pollEvent(event))
+	//	{
+	//		if (event.type == sf::Event::Closed)
+	//			Window.close();
+	//	}
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-		{
-			direction = 1;
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-		{
-			direction = 2;
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-		{
-			direction = 3;
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-		{
-			direction = 4;
-		}
-		
+	//	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	//	{
+	//		direction = 1;
+	//	}
+	//	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	//	{
+	//		direction = 2;
+	//	}
+	//	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	//	{
+	//		direction = 3;
+	//	}
+	//	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	//	{
+	//		direction = 4;
+	//	}
+	//	
 
-		switch (direction)
-		{
-		case 1: shape.move(0.f, -MOVEMENT_SPEED);
-			break;
-		case 2: shape.move(MOVEMENT_SPEED, 0.f);
-			break;
-		case 3: shape.move(0.f, MOVEMENT_SPEED);
-			break;
-		case 4: shape.move(-MOVEMENT_SPEED, 0.f);
-			break;
-		default:
-			break;
-		}
+	//	switch (direction)
+	//	{
+	//	case 1: shape.move(0.f, -MOVEMENT_SPEED);
+	//		break;
+	//	case 2: shape.move(MOVEMENT_SPEED, 0.f);
+	//		break;
+	//	case 3: shape.move(0.f, MOVEMENT_SPEED);
+	//		break;
+	//	case 4: shape.move(-MOVEMENT_SPEED, 0.f);
+	//		break;
+	//	default:
+	//		break;
+	//	}
 
-		Window.clear();
-		Window.draw(titleName);
-		//Window.draw(PacManSprite);
-		Window.draw(shape);
-		Window.display();
-	}
+	//	Window.clear();
+	//	Window.draw(titleName);
+	//	//Window.draw(PacManSprite);
+	//	Window.draw(shape);
+	//	Window.display();
+	//}
+
 
 	return 0;
 }
